dd9eb5f35f7fb649ddb388a2848169db68f805f5
J <j@tp.home>
2024-08-15 00:53:55 -0400

    broke into funcs w type hinting

--- main.py
+++ main.py
@@ -2,47 +2,66 @@
 import pandas as pd
 import os
 
-print("Starting Costco Inventory Counter!")
-
-print("Total = [ (L)ength * (W)idth * (H)eight ] + (E)xtra")
-
-counter = str(input("Current Counter: "))
-while True:
-    total = 0
-    item  = ""
-
+def handle_print_menu() -> int:
     while True:
         try:
             if os.name == "nt": os.system("cls")        #windows
             if os.name == "posix": os.system("clear")   #linux
-            choice = int(input("\nO P T I O N S\n\t(1) Add Item\n\t(2) Edit Item\n\t(3) Delete Item\n\t(4) Quit\n\n"))
+            choice: int = int(input("\nO P T I O N S\n\t(1) Add Item\n\t(2) Edit Item\n\t(3) Delete Item\n\t(4) Veiw Inventory\n\t(5) Quit\n\n"))
             break
         except:
             print("Bad Type Given!")
+    return choice
 
-    if choice == 1:
-        while True: #Getting values
-            try:
-                item   = str(input("Item: "))
-                length = int(input("L: "))
-                width  = int(input("W: "))
-                height = int(input("H: "))
-                extra  = int(input("E: "))
-                if length > 0 and width > 0 and height > 0 and extra >= 0:
-                    total = (length * width * height) + extra
-                else:
-                    print("Warning n <= 0!")
-                    pass
-                print(f'\n{item}: {total}')
-                break
-            except:
-                print("Bad Type Given!")
-    elif choice == 2:
-        pass
-    elif choice == 3:
-        pass
-    elif choice == 4:
-        print("Terminating Session!")
-        break
+def handle_input_and_calculation() -> int:
+    total       = 0
+    item        = str(input("Item: "))
+    length: int = int(input("L: "))
+    width:  int = int(input("W: "))
+    height: int = int(input("H: "))
+    extra:  int = int(input("E: "))
+    if length > 0 and width > 0 and height > 0 and extra >= 0:
+        total = (length * width * height) + extra
+    else:
+        print("Warning n <= 0!")
+    return total
+
+def main() -> None:
+    print("Starting Costco Inventory Counter!")
+    print("Total = [ (L)ength * (W)idth * (H)eight ] + (E)xtra")
+
+    counter: str = str(input("Current Counter: "))
+
+    while True:
+        total: int = 0
+        item:  str = ""
+
+        if choice := handle_print_menu() == 1:
+            while True: #Getting values
+                try:
+                    total = handle_input_and_calculation()
+                    print(f'\n{item}: {total}')
+                    break
+                except:
+                    print("Bad Type Given!")
+        elif choice == 2:
+            pass
+        elif choice == 3:
+            pass
+        elif choice == 4:
+            pass
+        elif choice == 5:
+            print("Terminating Session!")
+            break
+
+    return
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
 #use dict to store?
-#break into funcs?
+#Double check correct entry?
